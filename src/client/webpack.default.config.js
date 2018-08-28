2dc7295f07ffa91aa7ef3bf69111992561a0a22d
Patrick Muff <muff.patrick@muffag.ch>
2018-08-28 22:42:33 +0200

    Basic app structure

---
+++ src/client/webpack.default.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, '../build');
+
+module.exports = {
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          'ts-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=assets/[hash].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+              },
+              mozjpeg: {
+                progressive: true,
+                quality: 85
+              },
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loaders: [
+          'file-loader?name=assets/[hash].[ext]'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  performance: {
+    hints: false
+  }
+}
---
+++ src/client/webpack.development.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const defaultConfig = require('./webpack.default.config.js');
+
+// Directories
+const CLIENT_DIR = __dirname;
+
+module.exports = merge(defaultConfig, {
+  mode: 'development',
+  entry: path.join(CLIENT_DIR, 'app.tsx'),
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    new CleanWebpackPlugin(['build'], {
+      root: CLIENT_DIR,
+      verbose: true,
+      dry: false,
+      exclude: []
+    }),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'trunk - modern bittorrent network',
+      template: path.join(CLIENT_DIR, 'index.html')
+    }),
+    new WriteFilePlugin(),
+  ],
+  devServer: {
+    disableHostCheck: true
+  }
+});
