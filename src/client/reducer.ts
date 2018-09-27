a6ab1841fe17167ac038368801559d72b2463145
Patrick Muff <muff.patrick@muffag.ch>
2018-09-27 22:13:16 +0200

    Fix client that wasn't compiling anymore

--- src/client/reducer.ts
+++ src/client/reducer.ts
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 import { StateType } from 'typesafe-actions';
-import { authReducer } from './features/auth';
-import { postsReducer } from './features/posts';
-import { registerReducer } from './features/register';
-import { uploadReducer } from './features/upload';
+import authReducer from './features/auth/reducer';
+import postsReducer from './features/posts/reducer';
+import registerReducer from './features/register/reducer';
+import uploadReducer from './features/upload/reducer';
 
 const rootReducer = combineReducers({
   authReducer,
