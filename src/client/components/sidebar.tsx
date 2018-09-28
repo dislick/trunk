b6b75fab2b35130475152124061c3d4ad3cf450d
Patrick Muff <muff.pa@gmail.com>
2018-09-28 20:01:55 +0200

    Give feedback when copying the announce URL

--- src/client/components/sidebar.tsx
+++ src/client/components/sidebar.tsx
@@ -11,54 +11,89 @@ interface Props {
   onLogout: () => void;
 }
 
+interface State {
+  urlHasBeenCopied: boolean;
+}
+
 /**
  * Copies a string to clipboard using the navigator.clipboard API.
  * @param content
  */
-const copyToClipboard = async (content: string) => {
-  return (navigator as any).clipboard.writeText(content);
+const copyToClipboard = (content: string) => {
+  (navigator as any).clipboard.writeText(content);
 };
 
-export const Sidebar = (props: Props) => {
-  const formatBytes = (amount) => bytes(amount, { unitSeparator: ' ' });
+export class Sidebar extends React.Component<Props, State> {
+  private timeout;
 
-  return (
-    <section className='sidebar'>
-      <div className='sidebar-inner'>
-        <img src={require('../assets/trunk_logo.svg')} className='logo' />
+  public state: State = {
+    urlHasBeenCopied: false,
+  };
 
-        <div className='personal-area'>
-          {props.aboutMe &&
-            <div className='about-me'>
-              <p className='user'>{props.aboutMe.username}</p>
+  public formatBytes = (amount) => {
+    return bytes(amount, { unitSeparator: ' ' });
+  }
 
-              <table className='stats'>
-                <tbody>
-                  <tr>
-                    <td><p>up</p></td>
-                    <td><p>{formatBytes(props.aboutMe.total_uploaded)}</p></td>
-                  </tr>
-                  <tr>
-                    <td><p>down</p></td>
-                    <td><p>{formatBytes(props.aboutMe.total_downloaded)}</p></td>
-                  </tr>
-                  <tr>
-                    <td><p>ratio</p></td>
-                    <td><p>{getFormattedRatio(props.aboutMe.total_uploaded, props.aboutMe.total_downloaded)}</p></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          }
-          <Button
-            onClick={() => copyToClipboard(props.aboutMe.announceUrl)}
-            className='copy-announce-url'
-          >
-            Copy Announce URL
-          </Button>
-          <Button onClick={props.onLogout} className='logout'>Logout</Button>
+  public handleCopyClick = async () => {
+    copyToClipboard(this.props.aboutMe.announceUrl);
+    this.setState({ urlHasBeenCopied: true });
+
+    this.timeout = setTimeout(() => {
+      this.setState({ urlHasBeenCopied: false });
+    }, 5000);
+  }
+
+  public componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
+  public render() {
+    return (
+      <section className='sidebar'>
+        <div className='sidebar-inner'>
+          <img src={require('../assets/trunk_logo.svg')} className='logo' />
+
+          <div className='personal-area'>
+            {this.props.aboutMe &&
+              <div className='about-me'>
+                <p className='user'>{this.props.aboutMe.username}</p>
+
+                <table className='stats'>
+                  <tbody>
+                    <tr>
+                      <td><p>up</p></td>
+                      <td><p>{this.formatBytes(this.props.aboutMe.total_uploaded)}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p>down</p></td>
+                      <td><p>{this.formatBytes(this.props.aboutMe.total_downloaded)}</p></td>
+                    </tr>
+                    <tr>
+                      <td><p>ratio</p></td>
+                      <td><p>{getFormattedRatio(
+                        this.props.aboutMe.total_uploaded,
+                        this.props.aboutMe.total_downloaded,
+                      )}</p></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            }
+            <Button
+              onClick={this.handleCopyClick}
+              className='copy-announce-url'
+            >
+              {!this.state.urlHasBeenCopied
+                ?
+                'Copy Announce URL'
+                :
+                'Copied!'
+              }
+            </Button>
+            <Button onClick={this.props.onLogout} className='logout'>Logout</Button>
+          </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
